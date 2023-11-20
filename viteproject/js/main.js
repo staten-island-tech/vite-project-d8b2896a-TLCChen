5e63e4f57d325fbb9895f0d29aec9143f263f26d
TLCChen <139132998+TLCChen@users.noreply.github.com>
2023-11-20 13:14:49 -0500

    11-20-23:1.0

--- viteproject/js/main.js
+++ viteproject/js/main.js
@@ -1,23 +1,19 @@
+import { pokemons } from "./pokemon.js";
+import { DOMSselector } from "./doms.js";
 import "../css/style.css";
-import { pokemons } from "./pokemon";
-import { DOMSselector } from "./doms";
 
-function change(){
-  
-  document.querySelector(".btn").addEventListener("click", function(){
-    if(document.body.classList.contains("cool")){
-      document.body.classList.remove("cool")
-      document.body.classList.add("warm")
+function change() {
+  document.querySelector(".btn").addEventListener("click", function () {
+    if (document.body.classList.contains("cool")) {
+      document.body.classList.remove("cool");
+      document.body.classList.add("warm");
       document.querySelector(".btn").innerHTML = "Cool";
-    
-  }
-  else{
-    document.body.classList.remove("warm")
-    document.body.classList.add("cool")
-    document.querySelector(".btn").innerHTML = "Warm";
-
-  }
-  })
+    } else {
+      document.body.classList.remove("warm");
+      document.body.classList.add("cool");
+      document.querySelector(".btn").innerHTML = "Warm";
+    }
+  });
 }
 
 function cardCreator(name, type, img, description) {
@@ -25,9 +21,11 @@ function cardCreator(name, type, img, description) {
     //make this a DOMSselector object later.
     "beforeend",
     `<div class="item">
-      <h5 class = "text">${name}</h5>
-      <img src=${img} alt="cat1">
-      <p class = "text2">Type: ${type.map((upper) => upper.charAt(0).toUpperCase() + upper.slice(1)).join(" | ")}</p>
+      <h2 class = "text">${name}</h2>
+      <img src=${img} alt="This is ${name}">
+      <p class = "text2">Type: ${type
+        .map((upper) => upper.charAt(0).toUpperCase() + upper.slice(1))
+        .join(" | ")}</p>
       <p class ="description">${description}</p>
     </div>`
   );
@@ -35,12 +33,7 @@ function cardCreator(name, type, img, description) {
 
 function callCard() {
   pokemons.forEach((take) =>
-    cardCreator(
-      take.name,
-      take.type,
-      take.imgURL,
-      take.description
-    )
+    cardCreator(take.name, take.type, take.imgURL, take.description)
   );
 }
 
@@ -48,21 +41,23 @@ function filterType(power) {
   const typeFilter = pokemons.filter((pokemonTypes) =>
     pokemonTypes.type.includes(power)
   );
-  typeFilter.forEach((call) => cardCreator(call.name, call.type, call.imgURL, call.description));
+  typeFilter.forEach((call) =>
+    cardCreator(call.name, call.type, call.imgURL, call.description)
+  );
 }
-callCard()
-change()
+callCard();
+change();
 
 DOMSselector.button.forEach((press) =>
   press.addEventListener("click", function () {
-    DOMSselector.box.replaceChildren()
+    DOMSselector.box.replaceChildren();
     filterType(press.id);
   })
 );
 
-DOMSselector.allcards.addEventListener("click", function(){
-  DOMSselector.box.replaceChildren()
-  callCard()
-})
+DOMSselector.allcards.addEventListener("click", function () {
+  DOMSselector.box.replaceChildren();
+  callCard();
+});
 
 // Make cards that look like this: https://assets.pokemon.com/assets/cms2/img/cards/web/XY6/XY6_EN_15.png
