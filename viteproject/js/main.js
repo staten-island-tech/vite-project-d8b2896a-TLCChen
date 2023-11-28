fdd9460e3bd5bbbe7af5cf254b75f2cfa34f3307
TLCChen <139132998+TLCChen@users.noreply.github.com>
2023-11-27 19:48:20 -0500

    11-27-23

--- viteproject/js/main.js
+++ viteproject/js/main.js
@@ -1,7 +1,10 @@
+// import code from other files
 import { pokemons } from "./pokemon.js";
 import { DOMSselector } from "./doms.js";
 import "../css/style.css";
 
+// function that changes the color theme of the website when the event "click" is detected on the button "btn".
+// The text on the button changes each time the button is pressed using innerHTML. 
 function change() {
   document.querySelector(".btn").addEventListener("click", function () {
     if (document.body.classList.contains("cool")) {
@@ -15,7 +18,7 @@ function change() {
     }
   });
 }
-
+// This function takes in four arguements: name, type, img, and description, to create and add flex cards into the HTML.
 function cardCreator(name, type, img, description) {
   DOMSselector.box.insertAdjacentHTML(
     //make this a DOMSselector object later.
@@ -31,13 +34,15 @@ function cardCreator(name, type, img, description) {
     `
   );
 }
-
+// This function uses the function cardCreator to create flex cards using the objects from the array pokemons.
+// It does this by looping through each object in the array "pokemons" and taking the values in its variables to create flex cards using cardCreator function.
 function callCard() {
   pokemons.forEach((take) =>
     cardCreator(take.name, take.type, take.imgURL, take.description)
   );
 }
-
+// This function filters the pokemons array by type by taking in an arguement called power to create a new filtered array
+// called typeFilter. Then it creates flex cards using the function cardCreator using objects from the array typeFilter. 
 function filterType(power) {
   const typeFilter = pokemons.filter((pokemonTypes) =>
     pokemonTypes.type.includes(power)
@@ -46,16 +51,21 @@ function filterType(power) {
     cardCreator(call.name, call.type, call.imgURL, call.description)
   );
 }
+// Calls the functions listed below.
 callCard();
 change();
-
+// takes the many variables named "button" from the object DOMSselector. It then listens for "clicks" that are directed the "button"s.
+// Once the "click" is detected all cards on the screen are replaced with ""(an empty string).
+// Then the function filterType is used to filter the cards based on the id of the button pressed. The id of the buttons correspond to the type being filtered. 
 DOMSselector.button.forEach((press) =>
   press.addEventListener("click", function () {
     DOMSselector.box.replaceChildren();
     filterType(press.id);
   })
 );
-
+// Takes the variable "allcards" which is a button from the object DOMSselector. Then it detects for "clicks" directed at the "allcards" button.
+// After the "click" is detected all children of box is replaced with ""(all cards are deleted).
+// The function allCard is then called. 
 DOMSselector.allcards.addEventListener("click", function () {
   DOMSselector.box.replaceChildren();
   callCard();
